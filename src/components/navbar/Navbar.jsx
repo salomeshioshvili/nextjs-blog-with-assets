94e6aac83da96e7236a333d366590603e23a6905
salome shioshvili <79744349+salomeshioshvili@users.noreply.github.com>
2023-11-30 12:35:27 +0400

    Adding Routing

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,31 +1,38 @@
 import React from "react";
-import styles from './navbar.module.css'
+import styles from "./navbar.module.css";
+import Link from "next/link";
 
 function Navbar() {
   const links = [
     {
       id: 1,
       title: "Home",
+      url: '/'
     },
     {
       id: 2,
       title: "Portfolio",
+      url: '/portfolio'
     },
     {
       id: 3,
       title: "Blog",
+      url: '/blog'
     },
     {
       id: 4,
       title: "About",
+      url: '/about'
     },
     {
       id: 5,
       title: "Contact",
+      url: '/contact'
     },
     {
       id: 6,
       title: "Dashboard",
+      url: '/dashboard'
     },
   ];
 
@@ -33,7 +40,9 @@ function Navbar() {
     <div className={styles.container}>
       <div>Main</div>
       <div className={styles.links}>
-        {links.map((link) => (<div>{link.title}</div>))}
+        {links.map((link) => (
+          <Link href={link.url}>{link.title}</Link>
+        ))}
       </div>
     </div>
   );
